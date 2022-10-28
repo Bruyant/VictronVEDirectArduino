0b1ec51eb102d714ef45eb62b9091073e89e7d5a
Nicolas BRUYANT <nicolas.bruyant@lncmi.cnrs.fr>
2022-10-29 00:54:14 +0200

    soft serial and new labels

--- examples/ReadVEDirect/ReadVEDirect.ino
+++ examples/ReadVEDirect/ReadVEDirect.ino
@@ -8,56 +8,182 @@
 
  File: ReadVEDirect.ino / ReadVEDirect.cpp
  - Provides example use of the VEDirect library
+
+ 2020.04.10 - convert to SoftwareSerial
 ******************************************************************/
 
 #include "Arduino.h"
 #include "VEDirect.h"
+#include <LiquidCrystal.h>
+
+// Serial pins
+#define rxPin 3
+#define txPin 2
 
 // 32 bit ints to collect the data from the device
-int32_t VE_soc, VE_power, VE_voltage, VE_current;
+int32_t VE_soc, VE_power, VE_voltage, VE_current, VE_Vpv, VE_h20, VE_h22;
 // Boolean to collect an ON/OFF value
 uint8_t VE_alarm;
 
 // VEDirect instantiated with relevant serial object
-VEDirect myve(Serial3);
+VEDirect myve(rxPin, txPin);
+
+// select the pins used on the LCD panel
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+// define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+
+// read the buttons
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0);      // read the value from the sensor
+  if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 195)  return btnUP;
+  if (adc_key_in < 380)  return btnDOWN;
+  if (adc_key_in < 555)  return btnLEFT;
+  if (adc_key_in < 790)  return btnSELECT;
+
+
+
+  return btnNONE;  // when all others fail, return this...
+}
+
+void displayLCD(int Line) {  //display one line depending on line number
+  switch (Line) {
+      case 0:
+      {
+        lcd.print("Coucou Manou!     "); // print a simple message
+        break;
+      }
+      case 1:
+      {
+        lcd.print("V=");
+        lcd.print(VE_voltage, DEC);
+        lcd.print("mV P=");
+        lcd.print(VE_power, DEC);
+        lcd.print("W         ");
+        break;
+      }
+      case 2:
+      {  
+        lcd.print("SOC=");
+        lcd.print(VE_soc, DEC);
+        lcd.print(" I=");
+        lcd.print(VE_current, DEC);
+        lcd.print("A         ");
+        break;
+      }
+      case 3:
+      {  
+        lcd.print("Vpv=");
+        lcd.print(VE_Vpv, DEC);
+        lcd.print("V ALRM=");
+        lcd.print(VE_alarm, DEC);
+        lcd.print("         ");
+        break;
+      }
+      case 4:
+      {  
+        lcd.print("E=");
+        lcd.print(VE_h20, DEC);
+        lcd.print("Wh E-hier=");
+        lcd.print(VE_h22, DEC);
+        lcd.print("Wh         ");
+        break;
+      }
+  }  
+}
+
+
+int currLine=0;
 
 void setup() {
-	Serial.begin(9600);		// Adjust as needed
+	Serial.begin(19200);		// Adjust as needed - DEBUG serial port
+  myve.begin();
+  lcd.begin(16, 2);              // start the library
+  lcd.print(" BOOT ...");
+  Serial.print("Reading values from VE.Direct");
+  GetVE();
+  lcd.clear();
+  lcd.setCursor(0,0);
+  displayLCD(currLine);
+  lcd.setCursor(0,1);
+  displayLCD(currLine+1);
+
+}
+
+void GetVE() {
+	// Read the data 
+		VE_soc = myve.read(VE_FW);
+		VE_power = myve.read(VE_PPV);
+		VE_voltage = myve.read(VE_V);
+		VE_current = myve.read(VE_I);
+		VE_alarm = myve.read(VE_ERR);
+    VE_Vpv = myve.read(VE_VPV);
+    VE_h20 = myve.read(VE_H20); //(0.01 kWh) Yield today
+    VE_h22 = myve.read(VE_H22);// (0.01 kWh) Yield yesterday
 }
 
 void loop() {
-	Serial.println("Reading values from Victron Energy device using VE.Direct text mode");
-	Serial.println();
+  //Serial.println(currLine);
 
-	// Read the data
-	if(myve.begin()) {									// test connection
-		VE_soc = myve.read(VE_SOC);
-		VE_power = myve.read(VE_POWER);
-		VE_voltage = myve.read(VE_VOLTAGE);
-		VE_current = myve.read(VE_CURRENT);
-		VE_alarm = myve.read(VE_ALARM);
-	} else {
-		Serial.println("Could not open serial port to VE device");
-		while (1);
-	}
+  lcd_key = read_LCD_buttons();
 
-	// Print each of the values
-	Serial.print("State of Charge (SOC): ");
-	Serial.println(VE_soc, DEC);
-	Serial.print("Power:                 ");
-	Serial.println(VE_power, DEC);
-	Serial.print("Voltage                ");
-	Serial.println(VE_voltage, DEC);
-	Serial.print("Current                ");
-	Serial.println(VE_current, DEC);
-	Serial.print("Alarm                  ");
-	Serial.println(VE_alarm, DEC);
-	Serial.println();
-
-	// Copy the raw data stream (minus the \r) to Serial0
-	// Call read() with a token that won't match any VE.Direct labels
-	Serial.println("All data from device:");
-	myve.read(VE_DUMP);
-	Serial.println();
-	delay(10000);
+  switch (lcd_key) // depending on which button was pushed, we perform an action
+  {
+    case btnUP:
+    {
+      currLine= (currLine + 1) % 4;
+      delay(500);
+      break;
+    }
+    case btnDOWN:
+    {
+      currLine= (currLine - 1) % 4;
+      currLine= currLine * (currLine>0);
+      delay(500);
+      break;
+    }
+    case btnLEFT:
+    {
+      GetVE();
+      // Print each of the values
+      Serial.print("State of Charge (SOC): ");
+      Serial.println(VE_soc, DEC);
+      Serial.print("Power:                 ");
+      Serial.println(VE_power, DEC);
+      Serial.print("Voltage                ");
+      Serial.println(VE_voltage, DEC);
+      Serial.print("Current                ");
+      Serial.println(VE_current, DEC);
+      Serial.print("Alarm                  ");
+      Serial.println(VE_alarm, DEC);
+      Serial.println();
+      break;
+    }
+    case btnSELECT:
+    {
+      // Copy the raw data stream (minus the \r) to Serial0
+      // Call read() with a token that won't match any VE.Direct labels
+      Serial.println("All data from device:");
+      myve.read(VE_DUMP);
+      delay(500);
+      break;
+    }
+  }
+  //redraw screen
+  //GetVE();
+  lcd.setCursor(0,0);
+  displayLCD(currLine);
+  lcd.setCursor(0,1);
+  displayLCD(currLine+1);
 }
--- examples/ReadVEDirect/VEDirect.h
+++ examples/ReadVEDirect/VEDirect.h
@@ -1,1 +1,98 @@
-../../src/VEDirect.h
+/******************************************************************
+ VEDirect Arduino
+
+ Copyright 2018, 2019, Brendan McLearie
+ Distributed under MIT license - see LICENSE.txt
+
+ See README.md
+
+ File: VEDirect.h
+ - Class / enums / API
+
+ Updates:
+ - 2019-07-14:
+  	  - Rewrite of read - cleaner.
+  	  - Target labels extendible with enum and PROGMEM strings
+  	  - Retired copy_raw_to_serial0() code - use VE_DUMP on read
+  	  - Added some tunable parameters see #defines
+ - 2020-04-10:
+	  - Convert to SoftwareSerial so ESP8266 can use native usb serial for debug
+******************************************************************/
+
+#ifndef VEDIRECT_H_
+#define VEDIRECT_H_
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+// Tunable parameters - defaults tested on mega2560 R3
+#define VED_LINE_SIZE 30		 // Seems to be plenty. VE.Direct protocol could change
+#define VED_MAX_LEBEL_SIZE 6	 // Max length of all labels of interest + '\0'. See ved_labels[]
+#define VED_MAX_READ_LOOPS 60000 // How many read loops to be considered a read time-out
+#define VED_MAX_READ_LINES 50	 // How many lines to read looking for a value
+								 // before giving up. Also determines lines for diag dump
+#define VED_BAUD_RATE 19200
+
+// Extend this and ved_labels[] for needed inclusions// Extend this and ved_labels[] for needed inclusions
+enum VE_DIRECT_DATA {
+ VE_DUMP = 0,
+  VE_PID,
+  VE_FW,
+  VE_SER,
+  VE_V,
+  VE_I,
+  VE_VPV,
+  VE_PPV,
+  VE_Relay,
+  VE_OR,
+  VE_CS,
+  VE_ERR,
+  VE_LOAD,
+  VE_IL,
+  VE_H19,
+  VE_H20,
+  VE_H21,
+  VE_H22,
+  VE_H23,
+  VE_HSDS,
+  VE_MPPT,
+  VE_LAST_LABEL,
+};
+
+const char ved_labels[VE_LAST_LABEL][VED_MAX_LEBEL_SIZE] PROGMEM = {
+    "Dump",  // a string that won't match any label
+    "PID",     // Product ID
+    "FW",      // Firmware version (16 bit)
+    "SER#",    // Serial number
+    "V",       // (mV) Main or channel 1 (battery) voltage
+    "I",       // (mA) Main or channel 1 battery current
+    "VPV",     // (mV) Panel voltage
+    "PPV",     // (W) Panel power
+    "Relay",   // Relay state
+    "OR",      // Off reason
+    "CS",      // State of operation
+    "ERR",     // Error code
+    "LOAD",    // Load output state (ON/OFF)
+    "IL",      // (mA) Load current
+    "H19",     // (0.01 kWh) Yield total (user resettable counter)
+    "H20",     // (0.01 kWh) Yield today
+    "H21",     // (W) Maximum power today
+    "H22",     // (0.01 kWh) Yield yesterday
+    "H23",     // (W) Maximum power yesterday
+    "HSDS",    // Day sequence number (0..364)
+    "MPPT"    // Tracker operation mode
+};
+
+
+class VEDirect {
+public:
+	VEDirect(byte rxPin, byte txPin);
+	virtual ~VEDirect();
+	uint8_t begin();
+	int32_t read(uint8_t target);
+	void copy_raw_to_serial0(); // kept for backwards compatibility
+private:
+	SoftwareSerial& VESerial;
+};
+
+#endif /* VEDIRECT_H_ */
